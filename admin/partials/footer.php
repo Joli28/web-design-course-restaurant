e9266e448633f789cd2c39048c9e2ebc73c226ab
Arjoli <jolinjozugolli@gmail.com>
2021-10-15 11:59:06 +0200

    order-food

---
+++ admin/partials/footer.php
@@ -0,0 +1,8 @@
+
+    <div class="footer">
+       <div class="wrapper">
+         <p class="text-center">All rights reserved 2021</p>
+        </div>
+    </div>
+</body>
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Order Food</title>
+    <link rel="stylesheet" href="../css/admin.css" >
+</head>
+<body>
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+    </div>
